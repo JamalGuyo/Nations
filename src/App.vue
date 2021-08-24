da585cb0cbf398f71ac32883af041db8eb4aff6c
JamalGuyo <jamalguyo08@gmail.com>
2021-08-24 13:04:43 +0300

    create nav component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Nav />
+    <div class="content-wrapper"></div>
+  </div>
 </template>
 
 <script>
+import Nav from './components/Nav';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Nav,
+  },
 };
+//
 </script>
 
 <style>
@@ -15,5 +23,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+.content-wrapper {
+  width: 100%;
+  height: 100vh;
+  margin-left: 15rem;
+  background: pink;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ui left fixed vertical menu">
+    <div class="item">
+      <h1>
+        Location App
+      </h1>
+    </div>
+    <div class="item">
+      <div class="ui input">
+        <input type="text" placeholder="Search a country..." />
+      </div>
+    </div>
+    <div class="item">
+      <a class="item"> Countries <i class="far fa-flag"></i> </a>
+      <a class="item"> Regions <i class="fas fa-globe-africa"></i> </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style>
+.menu {
+  height: auto !important;
+  margin-right: 15rem;
+}
+a.item {
+  display: flex !important;
+  justify-content: space-between;
+}
+</style>
