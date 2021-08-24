d9a5248ee5fc94d10a184a1c080a8617f0b2253c
JamalGuyo <jamalguyo08@gmail.com>
2021-08-24 22:10:38 +0300

    remove unused values

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -135,7 +135,6 @@ export default {
         .finally(() => (this.loading = false));
     },
     validateNumber() {
-      let countryCode = this.country.callingCodes;
       let validation = /^(?:254|\+254|0)?(7(?:(?:[129][0–9])|(?:0[0–8])|(4[0–1]))[0–9]{6})$/.test(
         this.inputValue
       );
